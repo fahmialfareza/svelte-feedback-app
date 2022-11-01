fe5178afdeeb18335c7a946e2a15f7f1a1bf047d
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-11-01 11:48:17 +0700

    Learn Crash Course Svelte

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,22 @@
+<script>
+  // import { onDestroy, onMount } from "svelte";
+  import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+  import { FeedbackStore } from "../stores";
+
+  // let feedback = [];
+
+  // const unsubsribe = FeedbackStore.subscribe((data) => (feedback = data));
+
+  // onMount(() => {});
+
+  // onDestroy(() => {
+  //   unsubsribe();
+  // });
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={fb} />
+  </div>
+{/each}
